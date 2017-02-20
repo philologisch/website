a1cf35885177103fc87d92d7e1bf9f04f9a186ab
philologisch <philo1992@googlemail.com>
2017-02-20 23:19:06 +0100

    Menus Done
    -Menu in mobile works
    -Menu on desktop works

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
-var menuButton = document.getElementById('menuButton');
-var mynav = document.getElementById('navigation');
+var menuButton = document.getElementsByClassName('menuButton')[0];
+var mynav = document.getElementsByClassName('mobileMenu')[0];
 
 menuButton.onclick = function() {
-    if(mynav.className === 'inactive') {
-        mynav.className = '';
+    if(mynav.style.visibility === 'hidden') {
+        mynav.style.visibility = 'visible';
     } else {
-        mynav.className = 'inactive';
+        mynav.style.visibility = 'hidden';
     }
 }
