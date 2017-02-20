dc6ba6da56098454122ecb207956f63a50cf126f
philologisch <philo1992@googlemail.com>
2017-02-20 20:41:02 +0100

    rev 2.0
    Major changes:
    -slideshow works
    -and mino styling

---
+++ js/slideshow.js
@@ -0,0 +1,22 @@
+var prev = document.getElementsByClassName('prev')[0];
+var next = document.getElementsByClassName('next')[0];
+var i = 0;
+
+var slideImage = document.getElementsByClassName('slideimage')[0];
+var imageList = ['img/slide1.png', 'img/slide2.png', 'img/slide3.png'];
+
+next.onclick = function () {
+	i++;
+	if(i >= imageList.length) {
+		i = 0;
+	}
+	slideImage.setAttribute('src', imageList[i]);
+}
+
+prev.onclick = function () {
+	i--;
+	if(i < 0) {
+		i = imageList.length - 1;
+	}
+	slideImage.setAttribute('src', imageList[i]);
+}
